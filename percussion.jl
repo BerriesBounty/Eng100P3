a8dd933dabc68e5891897a37ca4d24a3d68f3031
Bryan Jiang <bryanjiang15@gmail.com>
2024-03-18 13:19:40 -0400

    Bass Drum

--- percussion.jl
+++ percussion.jl
@@ -8,7 +8,11 @@ using Plots;
 using DSP
 using Distributions
 using Random: seed!
+using Interpolations
+using SampledSignals
+using PortAudio
 
+#-------------experiments---------------------
 # function exp_freq(t, f_start, f_end, duration)
 #   k = log(f_end / f_start) / duration
 #   return f_start * exp(k * t)
@@ -33,38 +37,119 @@ using Random: seed!
 
 # amp = create_exponential_tone(10000, 10, 0.1, sample_rate)
 # sound(amp, sample_rate)
+#-------------experiments---------------------
+function bassDrum()
+  return bass_drum
+end
 
-function exponential_growth(f_start, f_end, duration, sample_rate)
-  t = 0:1/sample_rate:duration
-  A = f_start # starting value
-  growth_factor = log(f_end/f_start) / duration # calculated rate of growth
-  return A * exp.((t * growth_factor))
+#create an exponential function 
+function expon(start_val, end_val, dur, t)
+  return start_val * ((end_val / start_val) ^ (t / dur))
+end
+
+function expon_func(start_val, end_val, dur, sample_rate)
+  return expon.(start_val, end_val, dur, 0:1/sample_rate:dur)
+end
+
+#create an oscillator 
+function oscil(amp, freq, table_interp, t, sample_rate)
+    # Calculate the index for the table lookup with wrapping for periodicity
+    index = mod1(t * freq, length(table_interp))
+    return amp * table_interp(t * freq * sample_rate)
+    
+end
+
+function oscil_func(amp, table_interp, duration, sample_rate)
+  # Calculate the index for the table lookup with wrapping for periodicity
+  a1 = zeros(Float64, floor(Int, duration * sample_rate))
+  for t in 1:length(a1)
+    time = t / sample_rate
+    k1 = expon(120, 50, 0.2, time)
+    k2 = expon(500, 200, 0.4, time)
+    a1[t] = oscil(amp, k1, table_interp, time, sample_rate)
+    # Process `a1` such as writing to an output buffer
+    # ...
+  end
+  return a1
 end
 
 
-# Parameters for the signal
-f_start = 10000      # start frequency in Hz
-f_end = 2500        # end frequency in Hz
-duration = 0.1     # duration of the signal in seconds
+duration = 1    # duration of the signal in seconds
 sample_rate = 44100 # common sample rate for audio in Hz
+t = 0:1/sample_rate:duration
 
-kcutfreq = exponential_growth(10000, 2500, .1, sample_rate)
-amp = exponential_growth(10000, 10, .1, sample_rate)
+sine_wave_table = sin.(2 * π * (t))  # A simple sine wave table from 0 to 1
+sine_wave_interp = LinearInterpolation(1:length(sine_wave_table), sine_wave_table, extrapolation_bc=Periodic())
+
+function interpolate(table_interp, index)
+    # `index` can be a non-integer, and the interpolation scheme will handle it
+    return table_interp(index)
+end
+
+iamp = 100000
+
+k1 = expon_func(120, 50, 0.2, sample_rate)
+k2 = expon_func(500, 200, 0.4, sample_rate)
+bass_drum = oscil_func(iamp, sine_wave_interp, 0.25, sample_rate)
+#BASS DRUM SIGNAL
+
+
+kcutfreq = expon.(10000, 2500, .1, t)
+t = 0:1/sample_rate:duration
+amp = expon.(10000, 20, 0.1, t)
 
 seed!(0)
-arand = []
+hihat = []
 for i in 1:(4410)
-  global arand = [arand; ((rand()*2 - 1) * amp[i])]
+  global hihat = [hihat; ((rand()*2 - 1) * amp[i])]
+end
+#HIHAT SIGNAL
+
+function envelope(x; w::Int = 201) # uses moving average
+  h = (w-1) ÷ 2 # sliding window half-width (default 100)
+  x = abs.(x) # absolute value is crucial!
+  avg(v) = sum(v) / length(v) # function for (moving) average
+  return [avg(x[max(n-h,1):min(n+h,end)]) for n in 1:length(x)]
 end
 
-# responsetype = Lowpass(5; fs=50)
-# designmethod = Butterworth(2)
-# filt(digitalfilter(responsetype, designmethod), amp)
+#filter the signals TODO
+responsetype = Lowpass(5; fs=50)
+designmethod = Butterworth(2)
+filt(digitalfilter(responsetype, designmethod), amp)
 
-for i in 1:4
-  global arand = [arand; arand];
+# for i in 1:4
+#   global arand = [arand; arand];
+# end
+# soundsc(arand, sample_rate)
+t = 0:1/sample_rate:0.25
+t = t[1:floor(Int, 0.25*sample_rate)]
+env = (1 .- exp.(-80*t)) .* exp.(-30*t)
+bass_drum = env .* bass_drum
+
+
+ans = zeros(floor(Int, 0.25 * sample_rate))
+ans[1:4410] = hihat
+
+fullHihat = ans
+#ans .+= bass_drum
+
+result = [zeros(floor(Int, 2 * sample_rate))]
+
+buf = SampleBuf(ans, sample_rate)
+soundsc(ans, sample_rate)
+
+loop = zeros(2 * sample_rate)
+eigthNote = 11025
+for i in 1:8
+  loop[(i-1)*eigthNote+1:i*eigthNote] .+= fullHihat
 end
-soundsc(arand, sample_rate)
+for i in 1:2:8
+  loop[(i-1)*eigthNote+1:i*eigthNote] .+= bass_drum
+end
+soundsc(loop, sample_rate)
 
+wavwrite(loop, "touch.wav", Fs=sample_rate)
 
+x, S = wavread("touch.wav")
+soundsc(x, sample_rate)
 
--- test.jl
+++ test.jl
@@ -4,21 +4,59 @@ and routing it to the default audio output device using `write()`.
 =#
 
 using PortAudio: PortAudioStream, write
+using Gtk
+include("percussion.jl")
+const start_times = Dict{UInt32, UInt32}()
+
+playNote = false;
+index = 1;
+freql = [440, 480, 520, 560]
+
+w = GtkWindow("Key Press/Release Example")
+
+id1 = signal_connect(w, "key-press-event") do widget, event
+    k = event.keyval
+    if k ∉ keys(start_times)
+        start_times[k] = event.time # save the initial key press time
+        println("You pressed key ", k, " which is '", Char(k), "'.")
+        global playNote = true;
+        global  index = k%4 + 1
+    else
+        println(playNote)
+    end
+end
+
+id2 = signal_connect(w, "key-release-event") do widget, event
+    k = event.keyval
+    start_time = pop!(start_times, k) # remove the key from the dictionary
+    duration = event.time - start_time # key press duration in milliseconds
+    println("You released key ", k, " after time ", duration, " msec.")
+    global playNote = false;
+end
 
 stream = PortAudioStream(0, 1; warn_xruns=false)
 
 function play_tone(stream, freq::Real, duration::Real; buf_size::Int = 1024)
     S = stream.sample_rate
     current = 1
+    
     while current < duration*S
-        x = 0.7 * sin.(2π * (current .+ (1:buf_size)) * freq / S)
-        if(current > duration*S*0.5)
-          x = 0.7 * sin.(2π * (current .+ (1:buf_size)) * freq*2 / S)
+        println(playNote)
+        amplitude = 0.0001
+        freq1 = freql[index];
+        if(playNote)
+          amplitude = 0.7
         end
+        x = amplitude * sin.(2π * (current .+ (1:buf_size)) * freq1*2 / S)
         write(stream, x)
         current += buf_size
     end
     nothing
 end
 
-play_tone(stream, 440, 2)
+#play_tone(stream, 440, 20)
+
+PortAudioStream(0, 1; 44100) do stream
+    write(stream, bassDrum())
+  end
+

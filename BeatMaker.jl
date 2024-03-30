8b2ad283373fcd5f9a457d89ec9405fc5694e20a
Bryan Jiang <bryanjiang15@gmail.com>
2024-03-30 14:23:19 -0400

    Set up ways to switch between different grids

--- BeatMaker.jl
+++ BeatMaker.jl
@@ -3,15 +3,8 @@ using Sound: soundsc
 using WAV
 using PortAudio: PortAudioStream, write
 
-g = GtkGrid() # initialize a grid to hold buttons
-set_gtk_property!(g, :row_spacing, 5) # gaps between buttons
-set_gtk_property!(g, :column_spacing, 5)
-set_gtk_property!(g, :row_homogeneous, true) # stretch with window resize
-set_gtk_property!(g, :column_homogeneous, true)
-
 S = 44100
 N = 11025
-freqs = [67; 69; 71; 73]
 song = zeros(6*S) * ones(4)'
 
 stream = PortAudioStream(0, 1; warn_xruns=false)
@@ -24,9 +17,7 @@ percussion = [bass_drum, hi_hat, snare, crash]
 colors = ("red", "blue", "green", "purple")
 onoff = Bool.(zeros(16) * ones(4)')
 
-print(size(percussion))
-
-function miditone(idx::Int, note::Int, nsample::Int = N)
+function miditone(idx::Int, note::Int, g::GtkGrid, nsample::Int = N)
     x = percussion[note]
     write(stream, x) # play note so that user can hear it immediately
     if onoff[idx, note] == 0
@@ -45,25 +36,33 @@ function miditone(idx::Int, note::Int, nsample::Int = N)
 
 end
 
-for i in 1:16 # add the white keys to the grid
-    for n in 1:4
-    b = GtkButton() # make a button for this key
-    signal_connect((w) -> miditone(i, n), b, "clicked")
-    g[i, n] = b # put the button in row 2 of the grid
-    end
-end
-
 function play()
     beat = song[:,1] + song[:,2] + song[:,3] + song[:,4]
     write(stream, beat)
 end
 
-a = GtkButton("play")
-g[1,5] = a
-signal_connect((w) -> play(), a, "clicked")
+function getGrid()
+    g = GtkGrid() # initialize a grid to hold buttons
+    set_gtk_property!(g, :row_spacing, 5) # gaps between buttons
+    set_gtk_property!(g, :column_spacing, 5)
+    set_gtk_property!(g, :row_homogeneous, true) # stretch with window resize
+    set_gtk_property!(g, :column_homogeneous, true)
 
-win = GtkWindow("Beat Maker",400, 300)
-push!(win, g)
-showall(win)
+    for i in 1:16 # add the white keys to the grid
+        for n in 1:4
+        b = GtkButton() # make a button for this key
+        signal_connect((w) -> miditone(i, n, g), b, "clicked")
+        g[i, n] = b # put the button in row 2 of the grid
+        end
+    end
 
-print(size(song))
+    a = GtkButton("play")
+    g[1,5] = a
+    signal_connect((w) -> play(), a, "clicked")
+
+    return g
+
+# win = GtkWindow("Beat Maker",400, 300)
+# push!(win, g)
+# showall(win)
+end
--- mixing.jl
+++ mixing.jl
@@ -7,24 +7,31 @@ using PortAudio: PortAudioStream, write
 using Gtk
 using Plots;
 using Sound: soundsc
-include("instruments.jl")
+include("BeatMaker.jl")
 using Gtk
 
 win = GtkWindow("Sliders", 500, 200)
 g = GtkGrid()
 
-g_style = GtkCssProvider(data="#wb {background:blue;}")
-push!(GAccessor.style_context(g), GtkStyleProvider(g_style), 600)
-set_gtk_property!(g, :name, "wb") # set "style" of undo key
+# g_style = GtkCssProvider(data="#wb {background:blue;}")
+# push!(GAccessor.style_context(g), GtkStyleProvider(g_style), 600)
+# set_gtk_property!(g, :name, "wb") # set "style" of undo key
 
-button1 = GtkButton()
-g[1,1] = button1
-set_gtk_property!(g, :column_homogeneous, true)
+g1 = getGrid()
+g2 = GtkGrid();
+
+button_switch_to_grid2 = Gtk.Button("Go to Grid 2")
+
+push!(g1, button_switch_to_grid2)
+
+function switch_to_grid2(widget)
+  hide(g1)
+  show(g2)
+end
+
+signal_connect(switch_to_grid2, button_switch_to_grid2, "clicked")
+
+g[1,1] = g1
+
 push!(win, g)
-
-g2 = GtkGrid()
-button2 = GtkButton()
-g2[1,1] = button2
-set_gtk_property!(g2, :column_homogeneous, true)
-push!(win, g2)
 showall(win)
